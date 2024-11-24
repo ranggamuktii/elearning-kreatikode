723c8297a643dfa54803e3c8f6cbf35273616885
Rangga Mukti <r4n994mukti@gmail.com>
2024-11-24 15:10:34 +0700

    SETUP route for App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,31 +1,25 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import AdminDashboard from './pages/admin/AdminDashboard';
+import AdminCourseEdit from './pages/admin/AdminCourseEdit';
+import AdminCourseAdd from './pages/admin/AdminCourseAdd';
+import Home from './pages/user/Home';
+import CoursePage from './pages/user/CoursePage';
 
-function App() {
-  const [count, setCount] = useState(0);
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
+    <Router>
+      <Routes>
+        {/* Public Routes */}
+        <Route path="/" element={<Home />} />
+        <Route path="/course/:id" element={<CoursePage />} />
+
+        {/* Admin Routes */}
+        <Route path="/admin/add" element={<AdminCourseAdd />} />
+        <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="/admin/edit/:id" element={<AdminCourseEdit />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
