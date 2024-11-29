e39f39d006e75c9599a3a02f150eaa4200b86e80
Rangga Mukti <r4n994mukti@gmail.com>
2024-11-29 14:55:12 +0700

    ADD custom colors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,27 @@
 /** @type {import('tailwindcss').Config} */
+
+import flowbite from 'flowbite-react';
+
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}', flowbite.content()],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary-500': '#0089E2',
+        'primary-400': '#3BB1ED',
+        'primary-300': '#62CDF6',
+        'primary-200': '#97E6FC',
+        'primary-100': '#CAF5FD',
+        'secondary-500': '#0A2C50',
+        'secondary-400': '#336896',
+        'secondary-300': '#609ECA',
+        'secondary-200': '#9ACEED',
+        'secondary-100': '#CBE8F6',
+      },
+      backgroundImage: {
+        'gradation-blue': 'linear-gradient(to right, #0089E2, #62CDF6)',
+      },
+    },
   },
-  plugins: [],
+  plugins: [flowbite.plugin()],
 };
