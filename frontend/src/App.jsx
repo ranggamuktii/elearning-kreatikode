da65215d85a78727bceceeab88463e5c0c148aef
Amar Al Farizi <akunsaya3123@gmail.com>
2024-11-30 20:44:59 +0700

    add banner

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './components/Auth/Login'; // Ini nanti ganti aja ya sama component yang mau di coba
+// import Login from './components/Auth/Login'; // Ini nanti ganti aja ya sama component yang mau di coba
+import Banner from './components/Landing Page/Banner';
+// import Faq from './components/Landing Page/Faq';
 import './App.css';
+import 'flowbite-react';
 
 function App() {
   return (
     <Router>
       <Routes>
         {/* Ini juga ganti ya sama component yang mau di coba */}
-        <Route path="/login" element={<Login />} />
+        <Route path="/banner" element={<Banner />} />
       </Routes>
     </Router>
   );
---
+++ frontend/src/components/Landing Page/Banner.jsx
@@ -0,0 +1,60 @@
+import React ,{ useState } from "react";
+import {BsChevronRight, BsChevronLeft, BsChevronCompactLeft, BsChevronCompactRight} from 'react-icons/bs';
+import {RxDotFilled} from 'react-icons/rx';
+
+function Banner() {
+    const slides = [
+        {
+            url:'https://images.unsplash.com/photo-1484788984921-03950022c9ef?q=80&w=1864&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+        },
+        {
+            url:'https://assets.skilvul.com/campaign/1726654178187.jpg'
+        },
+        {
+            url:'https://assets.skilvul.com/campaign/impact-report-modal-modal-1680152788643.png'
+        },
+        {
+            url:'https://assets.skilvul.com/campaign/1725847935805.jpg'
+        },
+
+    ];
+
+    const [currentIndex, setCurrent] = useState(0);
+
+    const nextSlide = () => {
+
+        setCurrent(currentIndex === slides.length - 1 ? 0 : currentIndex + 1);
+    };
+
+    const prevSlide = () => {
+        setCurrent(currentIndex === 0 ? slides.length - 1 : currentIndex - 1);
+    };
+
+
+    return (
+        <div className="max-w-[1400px] h-[780px] w-full m-auto py-16 px-4 relative group">
+           <div style={{ backgroundImage: `url(${slides[currentIndex].url})` }}className="w-full h-full rounded-2xl bg-center bg-cover duration-500"></div>
+        {/* Left Arrow */}
+        <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 hover:text-white cursor-pointer">
+            <BsChevronCompactLeft onClick={prevSlide} size={30} />
+        </div>
+        {/* Right Arrow */}
+        <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 hover:text-white cursor-pointer" >
+            <BsChevronCompactRight onClick={nextSlide} size={30} />
+        </div>
+            <div className="flex top-4 justify-center py-2">
+                {slides.map((slide, slideIndex) => (
+                    <div
+                        key={slideIndex}
+                        onClick={() => setCurrent(slideIndex)}
+                        className="text-2xl cursor-pointer"
+                    >
+                        <RxDotFilled />
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+}
+
+export default Banner;
