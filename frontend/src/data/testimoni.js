c3c8dbbae1b646919300a16be55f8d2b624feb7e
Rangga Mukti <r4n994mukti@gmail.com>
2024-12-01 15:26:37 +0700

    ADD testimoni data

---
+++ frontend/src/data/testimoni.js
@@ -0,0 +1,58 @@
+const testimoniData = [
+  {
+    id: 1,
+    profile: 'https://placehold.co/400',
+    name: 'Rangga Mukti',
+    job: 'Fullstack Web Vocasia',
+    title: 'Lorem Ipsum is simply',
+    rating: 5,
+    review: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy",
+  },
+  {
+    id: 2,
+    profile: 'https://placehold.co/400',
+    name: 'Rangga Mukti',
+    job: 'Fullstack Web Vocasia',
+    title: 'Lorem Ipsum is simply',
+    rating: 5,
+    review: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy",
+  },
+  {
+    id: 3,
+    profile: 'https://placehold.co/400',
+    name: 'Rangga Mukti',
+    job: 'Fullstack Web Vocasia',
+    title: 'Lorem Ipsum is simply',
+    rating: 5,
+    review: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy",
+  },
+  {
+    id: 4,
+    profile: 'https://placehold.co/400',
+    name: 'Rangga Mukti',
+    job: 'Fullstack Web Vocasia',
+    title: 'Lorem Ipsum is simply',
+    rating: 5,
+    review: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy",
+  },
+  {
+    id: 5,
+    profile: 'https://placehold.co/400',
+    name: 'Rangga Mukti',
+    job: 'Fullstack Web Vocasia',
+    title: 'Lorem Ipsum is simply',
+    rating: 5,
+    review: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy",
+  },
+  {
+    id: 6,
+    profile: 'https://placehold.co/400',
+    name: 'Rangga Mukti',
+    job: 'Fullstack Web Vocasia',
+    title: 'Lorem Ipsum is simply',
+    rating: 5,
+    review: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy",
+  },
+];
+
+export default testimoniData;
