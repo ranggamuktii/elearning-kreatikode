2645473326b5609ed98a0be262a0514c15539afa
Rangga Mukti <r4n994mukti@gmail.com>
2024-12-01 15:30:11 +0700

    ADD route App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './components/Auth/Login'; // Ini nanti ganti aja ya sama component yang mau di coba
+import Testimoni from './components/Landing Page/Testimoni';
 import './App.css';
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* Ini juga ganti ya sama component yang mau di coba */}
-        <Route path="/login" element={<Login />} />
+        <Route path="/testimoni" element={<Testimoni />} />
       </Routes>
     </Router>
   );
