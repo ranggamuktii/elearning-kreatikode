448b637e345ab2ef04834d76f859c77b145bb556
Rangga Mukti <r4n994mukti@gmail.com>
2024-12-02 13:21:07 +0700

    ADD layout for children props

---
+++ frontend/src/components/Layout/Layout.jsx
@@ -0,0 +1,19 @@
+import PropTypes from 'prop-types';
+import Navbar from './Navbar';
+import Footer from './Footer';
+
+const Layout = ({ children }) => {
+  return (
+    <div>
+      <Navbar />
+      <main className="flex-grow">{children}</main>
+      <Footer />
+    </div>
+  );
+};
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default Layout;
