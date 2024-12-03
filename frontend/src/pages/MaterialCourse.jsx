c0337dab9e94e6bb7b9f8d2d3f44cb80bf59a260
sitizakiyah <sitizakiyah235@gmail.com>
2024-12-03 19:54:00 +0700

    add material course page

---
+++ frontend/src/pages/MaterialCourse.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import CourseMaterial from '../components/Course/CourseMaterial';
+import CourseDetail from '../components/Course/CourseDetail';
+import ContactAdmin from '../components/Course/ContactAdmin';
+
+const MateriPage = () => {
+    return (
+        <div className="">
+            <ContactAdmin />
+            <div className="flex flex-1 m-9 gap-8">
+                <CourseMaterial />
+                <CourseDetail />
+            </div>
+        </div>
+    );
+};
+
+export default MateriPage;
