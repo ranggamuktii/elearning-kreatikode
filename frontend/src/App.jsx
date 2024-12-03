1762847e81fcfa523e72658083e36dcdeb0cc0e9
Rangga Mukti <r4n994mukti@gmail.com>
2024-12-03 19:58:29 +0700

    ADD route Faq on App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Login from './components/Auth/Login';
 import Register from './components/Auth/Register';
 import LogoSlider from './components/Landing Page/LogoSlider';
 import Faq from './components/Landing Page/Faq';
+import './App.css';
 
 function App() {
   return (
