059342687cfb00ef61789e7432221b2887487f87
Rangga Mukti <r4n994mukti@gmail.com>
2024-12-13 16:57:46 +0700

    UDPATE api frontend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,39 @@
 import axios from 'axios';
+import { getUserData, removeUserToken } from '../components/Utils/tokendata';
 
 const API = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 });
+
+API.interceptors.request.use((config) => {
+  const userData = getUserData();
+  if (userData) {
+    config.headers.Authorization = `Bearer ${userData.token}`;
+  }
+  return config;
+});
+
+API.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    switch (error.response?.status) {
+      case 401:
+        removeUserToken();
+        window.location.href = '/login';
+        break;
+      case 403:
+        break;
+      case 404:
+        break;
+      default:
+        break;
+    }
+    return Promise.reject(error);
+  }
+);
+
+export const registerUser = (userData) => API.post('/users/register', userData);
+export const loginUser = (credentials) => API.post('/users/login', credentials);
 
 export const fetchCourses = () => API.get('/courses');
 export const fetchCourseById = (id) => API.get(`/courses/${id}`);
