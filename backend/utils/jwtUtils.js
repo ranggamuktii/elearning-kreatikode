092b5132e113fa4b41542cd36f8208f87f163118
Rangga Mukti <r4n994mukti@gmail.com>
2024-12-13 16:56:16 +0700

    ADD jwt utils

---
+++ backend/utils/jwtUtils.js
@@ -0,0 +1,13 @@
+import jwt from 'jsonwebtoken';
+
+export const generateToken = (user) => {
+  return jwt.sign({ id: user._id, name: user.name, email: user.email, gender: user.gender, dateOfBirth: user.dateOfBirth, phone: user.phone }, process.env.JWT_SECRET, { expiresIn: '24h' });
+};
+
+export const verifyToken = (token) => {
+  try {
+    return jwt.verify(token, process.env.JWT_SECRET);
+  } catch (error) {
+    throw new Error('Token tidak valid');
+  }
+};
