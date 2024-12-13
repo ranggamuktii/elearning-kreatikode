ac629a4876ca0e1be8dbcba2327eb2631c2a843b
Rangga Mukti <r4n994mukti@gmail.com>
2024-12-13 09:22:08 +0700

    UPDATE spacing landing page

--- frontend/src/components/LandingPage/LandingPage.jsx
+++ frontend/src/components/LandingPage/LandingPage.jsx
@@ -22,14 +22,22 @@ function LandingPage() {
         <Loading />
       ) : (
         <div className="flex flex-col items-center">
-          <div className="flex flex-col w-full space-y-8 sm:space-y-15">
-            <Banner />
-            <LogoSlider />
-            <CourseList />
-            <Benefit />
-            <Testimoni />
-            <CallToAction />
-            <Faq />
+          <div className="flex flex-col w-full">
+            <div className="mb-4">
+              <Banner />
+            </div>
+
+            <div className="space-y-2">
+              <LogoSlider />
+              <CourseList />
+            </div>
+
+            <div className="space-y-8 mt-8">
+              <Benefit />
+              <Testimoni />
+              <CallToAction />
+              <Faq />
+            </div>
           </div>
         </div>
       )}
