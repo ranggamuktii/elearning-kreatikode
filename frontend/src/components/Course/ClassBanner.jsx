0f9e8454f44c03e997004728eafc742bba310790
Rangga Mukti <r4n994mukti@gmail.com>
2024-12-16 19:54:13 +0700

    FIX class banner

--- frontend/src/components/Course/ClassBanner.jsx
+++ frontend/src/components/Course/ClassBanner.jsx
@@ -3,28 +3,14 @@ import { getProgress } from '../../services/api';
 import { useEffect, useState } from 'react';
 import { showWarningToast } from '../Utils/toastUtils';
 
-const ClassBanner = ({ courses }) => {
+const ClassBanner = ({ courses, userDetails, isLoggedIn }) => {
   const [progress, setProgress] = useState(0);
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  let userDetails = {};
-
-  useEffect(() => {
-    const token = Cookies.get('TOKEN');
-    if (token) {
-      setIsLoggedIn(true);
-      userDetails = decodeJwt(token);
-    } else {
-      setIsLoggedIn(false);
-    }
-  }, []);
 
   useEffect(() => {
     const fetchProgress = async () => {
       const response = await getProgress(`${courses._id}`, userDetails.id);
       const data = response.data.data[0];
-
       setProgress(data);
-      console.log(progress);
     };
 
     fetchProgress();
@@ -34,8 +20,14 @@ const ClassBanner = ({ courses }) => {
     if (!isLoggedIn) {
       return showWarningToast('Please login to access this feature');
     }
-    if (progress) {
-      window.location.href = `/course/${courses._id}/materials/${progress.lastAccessedMaterial}`;
+
+    if (progress && progress.lastAccessedMaterial) {
+      const lastAccessedIndex = courses.materials.findIndex((material) => material._id === progress.lastAccessedMaterial);
+      if (lastAccessedIndex !== -1 && lastAccessedIndex < courses.materials.length - 1) {
+        window.location.href = `/course/${courses._id}/materials/${courses.materials[lastAccessedIndex + 1]._id}`;
+      } else {
+        window.location.href = `/course/${courses._id}/quiz`;
+      }
     } else {
       window.location.href = `/course/${courses._id}/materials/${courses.materials[0]._id}`;
     }
@@ -66,12 +58,5 @@ ClassBanner.propTypes = {
     description: PropTypes.string.isRequired,
   }).isRequired,
 };
-
-// ClassBanner.defaultProps = {
-//   courses: {
-//     title: '',
-//     description: '',
-//   },
-// };
 
 export default ClassBanner;
