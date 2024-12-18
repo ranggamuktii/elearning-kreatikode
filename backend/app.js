d90a45d4f6e2c59225c54a6a76c7293aa3441982
eldom11 <2210631170065@student.unsika.ac.id>
2024-12-18 14:43:39 +0700

    feat(security): add measures to prevent NoSQL injection and DDoS Attacks

--- backend/app.js
+++ backend/app.js
@@ -3,16 +3,28 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import routes from './routes/index.js';
 import connectDB from './config/mongodb.js';
+import mongoSanitize from 'express-mongo-sanitize';
+import { rateLimit } from 'express-rate-limit'
 
 dotenv.config();
 const app = express();
 const port = process.env.PORT;
+
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000,
+	limit: 100,
+	standardHeaders: 'draft-8',
+	legacyHeaders: false,
+})
 
 // Koneksi Database
 connectDB();
 
 app.use(cors());
 app.use(express.json());
+
+app.use(mongoSanitize());
+app.use(limiter)
 
 app.use('/api/thumbnail', express.static('public/thumbnail'));
 app.use('/api', routes);
