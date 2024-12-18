11dd9a664c5c5259f2940dd8530797f7b016f8a5
Rangga Mukti <r4n994mukti@gmail.com>
2024-12-18 09:31:16 +0700

    UPDATE styling quiz

--- frontend/src/components/Course/CourseQuiz.jsx
+++ frontend/src/components/Course/CourseQuiz.jsx
@@ -5,6 +5,7 @@ import { useParams, useNavigate } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { fetchQuizByCourse, addQuizScore, fetchCourseById, addProgress } from '../../services/api';
 import CompletionModal from '../Modal/quizModal';
+import { showErrorToast } from '../Utils/toastUtils';
 
 const QuizDisplay = () => {
   const { courseId } = useParams();
@@ -16,9 +17,9 @@ const QuizDisplay = () => {
   const [submitted, setSubmitted] = useState(false);
   const [totalScore, setTotalScore] = useState(0);
   const [showModal, setShowModal] = useState(false);
-  const [userDetails, setUserDetails] = useState({})
-  const navigate = useNavigate()
-  
+  const [userDetails, setUserDetails] = useState({});
+  const navigate = useNavigate();
+
   useEffect(() => {
     const token = Cookies.get('TOKEN');
     if (token) {
@@ -70,20 +71,20 @@ const QuizDisplay = () => {
   const handleSubmit = async () => {
     const allAnswered = quiz.questions.every((_, index) => userAnswers[index] !== undefined);
     if (!allAnswered) {
-      alert('Silakan jawab semua pertanyaan sebelum submit!');
+      showErrorToast('Silakan jawab semua pertanyaan sebelum submit!');
       return;
     }
-  
+
     const totalQuestions = quiz.questions.length;
     const pointsPerQuestion = totalQuestions > 0 ? 100 / totalQuestions : 0;
     let score = 0;
-  
+
     quiz.questions.forEach((question, index) => {
       if (userAnswers[index] === question.correctAnswer) {
         score += pointsPerQuestion;
       }
     });
-  
+
     setTotalScore(score);
 
     const lastMaterial = course.materials[course.materials.length - 1];
@@ -99,14 +100,18 @@ const QuizDisplay = () => {
     setShowModal(true);
     window.scrollTo(0, 0);
   };
-  
+
   const handleCloseModal = () => {
     setShowModal(false);
   };
 
   const handleBack = () => {
-    navigate(`/course/${courseId}`)
-  }
+    navigate(`/course/${courseId}`);
+  };
+
+  const getOptionLabel = (index) => {
+    return String.fromCharCode(65 + index);
+  };
 
   if (loading) {
     return <div>Loading...</div>;
@@ -135,12 +140,12 @@ const QuizDisplay = () => {
       {showModal && <CompletionModal skor={totalScore} courseId={courseId} onClose={handleCloseModal} />}
 
       {/* Judul kursus */}
-      {course && <h1 className="text-3xl font-bold mb-6 text-center">{course.title}</h1>}
+      {course && <h1 className="text-2xl sm:text-3xl font-bold mb-6 text-center">{course.title}</h1>}
 
       {/* Judul kuis */}
-      <h2 className="text-2xl font-bold mb-4">{quiz.title}</h2>
+      <h2 className="text-lg sm:text-xl text-center font-semibold mb-4">{quiz.title}</h2>
 
-      {submitted && <h3 className="text-lg font-semibold mb-4">{`Total Skor: ${totalScore.toFixed(2).split('.')[0]}/100`}</h3>}
+      {submitted && <h3 className="text-lg text-center font-semibold mb-4">{`Total Skor: ${totalScore.toFixed(2).split('.')[0]}/100`}</h3>}
       <ul className="space-y-4">
         {quiz.questions.map((question, index) => {
           const userAnswer = userAnswers[index];
@@ -149,31 +154,40 @@ const QuizDisplay = () => {
           const isUserAnswerIncorrect = submitted && userAnswer !== correctAnswer;
 
           return (
-            <li key={index} className="border p-4 rounded-lg">
+            <li key={index} className="border p-4 rounded-2xl">
               <div className="flex flex-nowrap justify-between items-start">
-                <h2 className="text-lg font-semibold">{`${index + 1}.) ${question.question}`}</h2>
+                <h2 className="text-base sm:text-lg font-medium sm:font-semibold">{`${index + 1}.) ${question.question}`}</h2>
                 {!submitted && <p className="text-sm text-gray-600 min-w-24 text-right pt-1">{`${skor} points`}</p>}
                 {submitted && <p className="mt-1 text-sm w-24 text-right">{isUserAnswerCorrect ? `${skor}/${skor}` : isUserAnswerIncorrect ? `0/${skor}` : ''}</p>}
               </div>
-              <ul className="mt-2 space-y-2">
+              <ul className="mt-5 space-y-2">
                 {question.options.map((option, optionIndex) => {
                   let optionClass = '';
+                  let labelClass = 'mr-3 font-semibold px-4 py-2 rounded-xl';
 
                   if (submitted) {
                     if (isUserAnswerIncorrect && optionIndex === userAnswer) {
-                      optionClass = 'bg-red-300 w-full p-2 rounded-md';
+                      optionClass = 'bg-red-100 border-red-600 w-full p-2 rounded-xl';
+                      labelClass += ' bg-red-200 text-white border-red-500';
                     }
                     if (optionIndex === correctAnswer) {
-                      optionClass = 'bg-green-300 w-full p-2 rounded-md';
+                      optionClass = 'bg-green-200 text-green-600 w-full p-2 rounded-xl';
+                      labelClass += ' bg-green-300 text-white border-green-600';
+                    }
+                    if (!isUserAnswerIncorrect && optionIndex !== correctAnswer) {
+                      optionClass = 'bg-gray-50 w-full p-2 rounded-xl';
+                      labelClass += ' bg-gray-100 text-gray-600 border-gray-200';
                     }
                   } else {
-                    optionClass = userAnswer === optionIndex ? 'w-full p-2 border-2 border-gray-500 rounded-md bg-gray-50' : 'bg-gray-100 w-full p-2 rounded-md';
+                    optionClass = userAnswer === optionIndex ? 'w-full p-2 border-2 border-secondary-100 rounded-xl bg-primary-100 text-primary-500 font-medium bg-opacity-50' : 'border border-gray-200 bg-gray-0 w-full p-2 rounded-xl';
+                    labelClass += userAnswer === optionIndex ? ' bg-primary-100 text-primary-500 border-primary-500' : ' text-gray-700 border border-gray-200';
                   }
 
                   return (
-                    <li key={optionIndex} className="flex w-full ">
-                      <button type="button" onClick={() => handleAnswerChange(index, optionIndex)} className={`flex ${optionClass} transition duration-200`} disabled={submitted}>
-                        <span className="ml-2 text-left">{option}</span>
+                    <li key={optionIndex} className="flex w-full">
+                      <button type="button" onClick={() => handleAnswerChange(index, optionIndex)} className={`flex items-center w-full ${optionClass} transition duration-200`} disabled={submitted}>
+                        <span className={labelClass}>{getOptionLabel(optionIndex)}</span>
+                        <span className="text-left">{option}</span>
                       </button>
                     </li>
                   );
@@ -183,15 +197,23 @@ const QuizDisplay = () => {
           );
         })}
       </ul>
-      {!submitted && (
-        <button type="button" onClick={handleSubmit} className="bg-blue-500 text-white px-4 py-2 rounded-lg w-full mt-4">
-          Submit
+      <div className="flex justify-between gap-10 mt-4">
+        <button type="button" onClick={handleBack} className="flex items-center justify-center gap-2 px-6 py-2 bg-primary-500 hover:bg-primary-600 text-white rounded-lg w-1/2 transition duration-200">
+          <svg className="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 19l-7-7m0 0l7-7m-7 7h18" />
+          </svg>
+          Kembali
         </button>
-      )}
 
-      <button type="button" onClick={handleBack} className="mt-4 bg-red-500 text-white px-4 py-2 rounded-lg w-full">
-        Kembali
-      </button>
+        {!submitted && (
+          <button type="button" onClick={handleSubmit} className="flex items-center justify-center gap-2 px-6 py-2 bg-green-500 hover:bg-green-600 text-white rounded-lg w-1/2 transition duration-200">
+            Submit
+            <svg className="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+            </svg>
+          </button>
+        )}
+      </div>
     </div>
   );
 };
